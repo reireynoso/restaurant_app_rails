40b6b16d10fc92e32086858826babfb5e982f6a0
reireynoso <reireynoso@gmail.com>
2020-01-01 02:23:40 -0500

    photo url update

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::UsersController < ApplicationController
     skip_before_action :require_login, only: [:create]
     def create
         # byebug
-        photo_url = Cloudinary::Uploader.upload(user_params[:photo_url])
+        # photo_url = Cloudinary::Uploader.upload(user_params[:photo_url])
+        photo_url = upload_image_to_cloudinary(user_params[:photo_url])
         user = User.create(user_params)
         user.update(photo_url: photo_url["url"])
         if user.valid?
@@ -25,10 +26,14 @@ class Api::V1::UsersController < ApplicationController
         # byebug
         # puts params[:photo_url] == nil
         if params[:username] != "" && params[:photo_url] != nil
-            session_user.update(username: params[:username], photo_url: params[:photo_url])
+            # delete existing photo on cloudinary
+            updated_photo_url = upload_image_to_cloudinary(params[:photo_url])
+            session_user.update(username: params[:username], photo_url: updated_photo_url)
             update_user_json
         elsif params[:username] == "" && params[:photo_url] != nil
-            session_user.update(photo_url: params[:photo_url])
+            # delete existing photo on cloudinary
+            updated_photo_url = upload_image_to_cloudinary(params[:photo_url])
+            session_user.update(photo_url: updated_photo_url)
             update_user_json
         elsif params[:username] != "" && params[:photo_url] == nil 
             existing_user = User.find_by(username: params[:username])
@@ -51,6 +56,10 @@ class Api::V1::UsersController < ApplicationController
     end
 
     private
+
+    def upload_image_to_cloudinary(image)
+        Cloudinary::Uploader.upload(image)
+    end
 
     def update_user_json
         render json: {user: UserSerializer.new(session_user), success: "reached regular update path"}
