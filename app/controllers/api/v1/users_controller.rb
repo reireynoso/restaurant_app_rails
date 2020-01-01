22a456b4902605c1992ac79f380e3998b42b5523
reireynoso <reireynoso@gmail.com>
2020-01-01 14:28:59 -0500

    user_controller refactored with destroy method for existing photo in cloudinary

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -23,31 +23,32 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def update_user
-        # byebug
-        # puts params[:photo_url] == nil
         if params[:username] != "" && params[:photo_url] != nil
-            # delete existing photo on cloudinary
-            updated_photo_url = upload_image_to_cloudinary(params[:photo_url])
-            session_user.update(username: params[:username], photo_url: updated_photo_url)
-            update_user_json
+            # existing_user = User.find_by(username: params[:username])
+            if existing_user?
+                # render json: {errors: ["Username is taken"]}
+                existing_user_render_error
+            else
+                updated_photo_url = remove_old_image_and_update_image(params[:photo_url])
+                session_user.update(username: params[:username], photo_url: updated_photo_url["url"])
+                update_user_json
+            end
         elsif params[:username] == "" && params[:photo_url] != nil
-            # delete existing photo on cloudinary
-            updated_photo_url = upload_image_to_cloudinary(params[:photo_url])
-            session_user.update(photo_url: updated_photo_url)
+            updated_photo_url = remove_old_image_and_update_image(params[:photo_url])
+            session_user.update(photo_url: updated_photo_url["url"])
             update_user_json
         elsif params[:username] != "" && params[:photo_url] == nil 
-            existing_user = User.find_by(username: params[:username])
-                if existing_user
-                    render json: {errors: ["Username is taken"]}
-                else
-                    session_user.update(username: params[:username])
-                    update_user_json
-                end
-            # puts session_user
+            # existing_user = User.find_by(username: params[:username])
+            if existing_user?
+                # render json: {errors: ["Username is taken"]}
+                existing_user_render_error
+            else
+                session_user.update(username: params[:username])
+                update_user_json
+            end
         else
-            render json: {errors: ["No changes made."]}
+            render json: {errors: ["No changes made since fields are blank."]}
         end
-        
     end
 
     def change_password
@@ -56,6 +57,20 @@ class Api::V1::UsersController < ApplicationController
     end
 
     private
+
+    def existing_user?
+        existing_user = User.find_by(username: params[:username])
+    end
+
+    def existing_user_render_error
+        render json: {errors: ["Username is taken"]}
+    end
+
+    def remove_old_image_and_update_image(image)
+        public_id = session_user.photo_url.split("/").slice(-1).split(".")[0]
+        Cloudinary::Uploader.destroy(public_id)
+        updated_photo_url = upload_image_to_cloudinary(image)
+    end
 
     def upload_image_to_cloudinary(image)
         Cloudinary::Uploader.upload(image)
